d1373d8ea1738cf2ae44e2103b767d8a97155813
Alejo2396 <alejomore2396@gmail.com>
2025-12-16 09:13:57 -0500

    fix: usar puerto dinámico de Railway

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', authRoutes);
 
-app.listen(4000, () => {
-    console.log('Servidor ejecutando en puerto 4000');
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutando en puerto ${PORT}`);
 });
